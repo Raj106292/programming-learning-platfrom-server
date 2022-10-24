8a83ba909f58bc405cbfec95d1d897cc22183b13
Raj106292 <rajdas.106292@gmail.com>
2022-10-24 22:11:08 +0600

    categories data loaded in server

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./Data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('Programming Learning Platform Server is Running');
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
 
 app.listen(port , () => {
     console.log(`server is running on port ${port}`);
