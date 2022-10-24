72490666e421e97dd1b2f04cfeb28b29905a4ee2
Raj106292 <rajdas.106292@gmail.com>
2022-10-24 22:36:28 +0600

    cors added to server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const categories = require('./Data/categories.json');
 
